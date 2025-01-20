4298251cf227a63c59ec6e605338483f63857542
Finlay Miller <finlay@dal.ca>
2025-01-20 16:28:05 -0400

    session with sageev, chandramouli, sri harsha

--- gen_all_embeddings.py
+++ gen_all_embeddings.py
@@ -51,7 +51,13 @@ class EmbeddingGenerator:
 
     def process(self, file_path: str) -> list[torch.tensor]:
         return self.processor(file_path)
-
+    def get_embeddings_tokenized(self, input_tokens):
+        tokens_mask = input_tokens > 0
+        tokens_encoded, tokens_mask = self.midi_encoder(
+                encoder_input_tokens=input_tokens, encoder_inputs_mask=tokens_mask
+            )
+        return tokens_encoded, tokens_mask
+        
     def get_embeddings(self, file_path: str) -> list[torch.tensor]:
         out = self.process(file_path)
         embeddings = []
@@ -63,7 +69,12 @@ class EmbeddingGenerator:
             tokens_encoded, tokens_mask = self.midi_encoder(
                 encoder_input_tokens=input_tokens, encoder_inputs_mask=tokens_mask
             )
-            embeddings.append(tokens_encoded.squeeze(0).cpu().detach())
+            # use token mask
+            # print(tokens_mask.shape)
+            # print(tokens_mask.sum(),tokens_encoded[tokens_mask].shape)
+            # raise Exception("testing")
+            embeddings.append(tokens_encoded[tokens_mask].mean(dim=0).cpu().detach())
+            break
 
         return embeddings
 
@@ -96,9 +107,11 @@ def main():
         n_files += len([f for f in files if f.endswith(supported_extensions)])
 
     p = Progress()
-    task = p.add_task(f"generating embeddings", total=n_files)
+    task1 = p.add_task(f"generating tokens", total=n_files)
+    task2 = p.add_task(f"generating embeddings", total=n_files)
     successful_writes = 0
     with p:
+        midi_tokens = dict()
         for path, _, files in os.walk(in_path):
             for file in [
                 os.path.join(path, f) for f in files if f.endswith(supported_extensions)
@@ -106,9 +119,25 @@ def main():
                 out_file_path = os.path.join(
                     out_path, os.path.splitext(os.path.basename(file))[0] + ".pt"
                 )
+                midi_tokens[out_file_path, file] = generator.process(file)
+                p.advance(task1)
+            #     if len(midi_tokens)==100:
+            #         break
+            # if len(midi_tokens)==100:
+            #     break
+        keys = list(midi_tokens.keys())
+        all_tokens = torch.cat([torch.IntTensor(midi_tokens[key][0]).view(1, -1) for key in keys])
+        print(all_tokens.shape)
+        BSZ = 8
+        for i in range(0,all_tokens.shape[0],BSZ):
+            batch = all_tokens[i:i+BSZ].cuda(device=generator.device)
+            with torch.autocast("cuda"):
+                tokens, tokens_mask = generator.get_embeddings_tokenized(batch)
+            for idx in range(batch.shape[0]):
+                out_file_path, file = keys[i+idx]
+                avg_embedding = tokens[idx][tokens_mask[idx]].mean(0)
+                
                 print(f"generating embedding for '{file}'")
-                midi_embeddings = generator.get_embeddings(file)
-                avg_embedding = np.mean(midi_embeddings, axis=0)
                 print(
                     f"\tshape: {avg_embedding.shape}, min: {avg_embedding.min()}, max: {avg_embedding.max()}, mean: {avg_embedding.mean()}"
                 )
@@ -118,7 +147,7 @@ def main():
                     print(f"\tRuntimeError while writing file, retrying...")
                     time.sleep(1)
                     midi_embeddings = generator.get_embeddings(file)
-                    avg_embedding = np.mean(midi_embeddings, axis=0)
+                    avg_embedding = midi_embeddings[0] #np.mean(midi_embeddings, axis=0)
                     print(
                         f"\tshape: {avg_embedding.shape}, min: {avg_embedding.min()}, max: {avg_embedding.max()}, mean: {avg_embedding.mean()}"
                     )
@@ -135,7 +164,7 @@ def main():
                         f"\t{os.path.getsize(out_file_path)} bytes written to {out_file_path}"
                     )
 
-                p.advance(task)
+                p.advance(task2)
 
     print(f"embedding generation complete")
     print(
